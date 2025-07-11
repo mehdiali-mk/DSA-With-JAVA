08bec21e112f4ed2bafed96d2b7733bb7402ef56
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-07-11 17:09:53 +0530

    JAVA Basic Folder is added.

---
+++ JAVA Basics/_01_HelloWorld.java
@@ -0,0 +1,10 @@
+public class _01_HelloWorld {
+    public static void main(String args[]){
+        System.out.print("Hello World!\nMehdiali Kadiwala\n");
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
---
+++ JAVA Basics/_02_SumOfTwo.java
@@ -0,0 +1,8 @@
+public class _02_SumOfTwo {
+    public static void main(String args[]) {
+        int num1 = 4;
+        int num2 = 5;
+
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+    }
+}
