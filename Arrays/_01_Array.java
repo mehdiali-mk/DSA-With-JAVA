6e0f6b459eb85a327a9bc86627af74b5e6a286a3
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-07-24 14:59:37 +0530

    Basic Array program is added.

---
+++ Arrays/_01_Array.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class _01_Array {
+    public static void main(String args[]) {
+        int integerArray[] = { 1, 2, 3, 4, 5, 6 };
+
+        System.out.println("\n");
+        for (int i = 0; i < integerArray.length; i++) {
+            System.out.println("integerArray[" + i + "] = " + integerArray[i]);
+        }
+        System.out.println("\n\n");
+    }
+}
