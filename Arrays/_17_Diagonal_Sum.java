7970d44dece50fcbef44e23c6ec3730a926f9407
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-08-13 18:56:22 +0530

    Diagonal Sum + Optimized programs are added.

---
+++ Arrays/_17_Diagonal_Sum.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class _17_Diagonal_Sum {
+    public static int DiagonalSum(int matrix[][]) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == j) {
+                    sum += matrix[i][j];
+                } else if (i + j == matrix.length - 1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+
+        System.out.println("\n\nSum of Diagonals = " + DiagonalSum(matrix) + "\n\n");
+    }
+}
