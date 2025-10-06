2676313ea8f5f46a69fa38354fa855e4c0e24e6c
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-10-06 18:02:44 +0530

    Question in Number Combination is added.

--- Backtracking/_08_Number_Combinations.java
+++ Backtracking/_08_Number_Combinations.java
@@ -1,3 +1,18 @@
+/*
+ *  Keypad Combinations
+    Given a string containing digits from 2-9 inclusive, print all possible letter combinations that the number could represent. You can print the answer in any order. A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+    Sample Input 1 : digits = "23"
+    Sample Output 1 : "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"
+    
+    Sample Input 2 : digits = "2"
+    Sample Output 2 : "a", "b", "c"
+
+    Sample Input 3 : digits = ""
+    Sample Output 3 : ””
+
+ */
+
 package Backtracking;
 
 public class _08_Number_Combinations {
