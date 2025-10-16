0776e159dce6ca4be2960b42b5e76ec8a9937fa6
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-10-16 10:22:54 +0530

    Monotonic ArrayList program is added.

---
+++ Array List/_10_Monotonic_ArrayList.java
@@ -0,0 +1,47 @@
+/*
+ * Monotonic ArrayList (EASY)
+An Arraylist is monotonic if it is either monotone increasing or monotone decreasing.
+An Arraylist nums is monotone increasing if for all i <= j, nums.get(i) <= nums.get(j). An
+Arraylist nums is monotone decreasing if for all i <= j, nums.get(i) >= nums.get(j).
+Given an integer Arraylist nums, return true if the given list is monotonic, or false otherwise.
+Sample Input 1 : nums = [1,2,2,3]
+Sample Output 1 : true
+Sample Input 2 : nums = [6,5,4,4]
+Sample Output 2 : true
+Sample Input 3 : nums = [1,3,2]
+Sample Output 3 : false
+Constraints :
+● 1 <= nums.size() <= 105
+● -105 <= nums.get(i) <= 105
+ * 
+ */
+
+import java.util.ArrayList;
+
+public class _10_Monotonic_ArrayList {
+    public static boolean isMonotonic(ArrayList<Integer> myArrayList) {
+        boolean increasingMonotonic = true;
+        boolean decreasingMonotonic = true;
+
+        for (int i = 0; i < myArrayList.size() - 1; i++) {
+            if (myArrayList.get(i) > myArrayList.get(i + 1))
+                increasingMonotonic = false;
+            if (myArrayList.get(i) < myArrayList.get(i + 1))
+                decreasingMonotonic = false;
+        }
+
+        return increasingMonotonic || decreasingMonotonic;
+    }
+
+    public static void main(String args[]) {
+        ArrayList<Integer> myArrayList = new ArrayList<>();
+        myArrayList.add(1);
+        myArrayList.add(2);
+        myArrayList.add(2);
+        myArrayList.add(3);
+
+        System.out.println("\n\nArrayList:\n" + myArrayList);
+        System.out.println("\nisMonotonic = " + isMonotonic(myArrayList) + "\n\n\n");
+
+    }
+}
