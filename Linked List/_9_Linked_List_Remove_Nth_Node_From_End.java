7f6bb3d7afb14a44a07edca50c62abcb8d64c24a
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-11-09 09:43:23 +0530

    Linked List Remove Nth Node program is added.

---
+++ Linked List/_9_Linked_List_Remove_Nth_Node_From_End.java
@@ -0,0 +1,205 @@
+import org.w3c.dom.Node;
+
+public class _9_Linked_List_Remove_Nth_Node_From_End {
+    public static class Node {
+        int data;
+        Node nextNode;
+
+        public Node(int data) {
+            this.data = data;
+            this.nextNode = null;
+        }
+    }
+
+    public static Node headNode;
+    public static Node tailNode;
+    public static int size = 0;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (headNode == null) {
+            headNode = tailNode = newNode;
+            size++;
+            return;
+        }
+
+        newNode.nextNode = headNode;
+        headNode = newNode;
+        size++;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if (headNode == null) {
+            headNode = tailNode = newNode;
+            size++;
+            return;
+        }
+
+        tailNode.nextNode = newNode;
+        tailNode = newNode;
+        size++;
+    }
+
+    public void displayLinkedList() {
+        Node tempNode = headNode;
+
+        while (tempNode != null) {
+            if (tempNode != headNode) {
+                System.out.print(" -> ");
+            }
+            System.out.print(tempNode.data);
+            tempNode = tempNode.nextNode;
+        }
+        System.out.println("\n");
+    }
+
+    public void addIndex(int index, int data) {
+        if (index == 0) {
+            addFirst(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+        Node tempNode = headNode;
+        size++;
+        int i = 0;
+
+        while (i < index - 1) {
+            tempNode = tempNode.nextNode;
+            i++;
+        }
+
+        newNode.nextNode = tempNode.nextNode;
+        tempNode.nextNode = newNode;
+    }
+
+    public static int removeFirst() {
+        if (size == 0) {
+            System.out.println("\nLinked List is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int value = headNode.data;
+            headNode = tailNode = null;
+            size = 0;
+            return value;
+        }
+
+        int value = headNode.data;
+        headNode = headNode.nextNode;
+        size--;
+        return value;
+    }
+
+    public static int removeLast() {
+        if (size == 0) {
+            System.out.println("\nLinked List is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int value = headNode.data;
+            headNode = tailNode = null;
+            size = 0;
+            return value;
+        }
+
+        Node tempNode = headNode;
+
+        for (int i = 0; i < size - 2; i++) {
+            tempNode = tempNode.nextNode;
+        }
+
+        int value = tempNode.nextNode.data;
+        tempNode.nextNode = null;
+        tailNode = tempNode;
+        size--;
+        return value;
+    }
+
+    public static int iterativeSearch(int key) {
+        Node tempNode = headNode;
+        for (int i = 0; i < size - 1; i++) {
+            if (tempNode.data == key) {
+                return i;
+            }
+            tempNode = tempNode.nextNode;
+        }
+        return -1;
+    }
+
+    public static int recursiveHelper(Node newHeadNode, int key) {
+        if (newHeadNode == null) {
+            return -1;
+        }
+
+        if (newHeadNode.data == key) {
+            return 0;
+        }
+
+        int index = recursiveHelper(newHeadNode.nextNode, key);
+
+        if (index == -1) {
+            return -1;
+        }
+
+        return index + 1;
+    }
+
+    public static int recursiveSearch(int key) {
+        return recursiveHelper(headNode, key);
+    }
+
+    public static void reverseIterative() {
+        Node previousNode = null;
+        Node currentNode = tailNode = headNode;
+        Node nextCurrentNode;
+
+        while (currentNode != null) {
+            nextCurrentNode = currentNode.nextNode;
+            currentNode.nextNode = previousNode;
+            previousNode = currentNode;
+            currentNode = nextCurrentNode;
+        }
+        headNode = previousNode;
+    }
+
+    public static int removeNthFromEnd(int nthNode) {
+        if (nthNode == size) {
+            int value = headNode.data;
+            headNode = headNode.nextNode;
+            size--;
+            return value;
+        }
+
+        Node currentNode = headNode;
+        for (int i = 0; i < (size - nthNode) - 1; i++) {
+            currentNode = currentNode.nextNode;
+        }
+
+        int value = currentNode.nextNode.data;
+        currentNode.nextNode = currentNode.nextNode.nextNode;
+        size--;
+        return value;
+    }
+
+    public static void main(String args[]) {
+        _9_Linked_List_Remove_Nth_Node_From_End myLinkedList = new _9_Linked_List_Remove_Nth_Node_From_End();
+
+        myLinkedList.addLast(1);
+        myLinkedList.addLast(2);
+        myLinkedList.addLast(3);
+        myLinkedList.addLast(4);
+        myLinkedList.addLast(5);
+        System.out.println("\n\nLinked List of size (" + myLinkedList.size + "):");
+        myLinkedList.displayLinkedList();
+
+        removeNthFromEnd(3);
+        removeNthFromEnd(2);
+
+        System.out.println("\n\nLinked List of size (" + myLinkedList.size + "):");
+        myLinkedList.displayLinkedList();
+
+        System.out.println("\n");
+    }
+}
