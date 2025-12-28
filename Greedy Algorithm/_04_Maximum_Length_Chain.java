b64c3830a36d93a90782617dc9c4b7e1720d877a
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-12-28 13:01:32 +0530

    Maximum Length Chain program is added.

---
+++ Greedy Algorithm/_04_Maximum_Length_Chain.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class _04_Maximum_Length_Chain {
+    public static void main(String args[]) {
+        int pairs[][] = { { 5, 24 }, { 39, 60 }, { 5, 28 }, { 27, 40 }, { 50, 90 } };
+
+        Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+
+        int chainLength = 1;
+        int chainEnd = pairs[0][1];
+
+        for (int i = 0; i < pairs.length; i++) {
+            if (pairs[i][0] > chainEnd) {
+                chainLength++;
+                chainEnd = pairs[i][1];
+            }
+        }
+
+        System.out.println("\n\n\nMaximum Chain Length = " + chainLength + "\n\n\n");
+    }
+}
