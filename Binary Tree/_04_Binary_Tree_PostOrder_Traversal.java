8813ca47b72652bebb201952aca8012e711fe746
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2026-01-12 15:06:52 +0530

    Binary Tree Post Order Traversal java program is added.

---
+++ Binary Tree/_04_Binary_Tree_PostOrder_Traversal.java
@@ -0,0 +1,63 @@
+public class _04_Binary_Tree_PostOrder_Traversal {
+    static class Node {
+        int data;
+        Node leftNode;
+        Node rightNode;
+
+        Node(int data) {
+            this.data = data;
+            leftNode = null;
+            rightNode = null;
+        }
+    }
+
+    public static Node buildBinaryTree(int preOrder[], int inOrder[], int[] preOrderIndex, int leftBound,
+            int rightBound) {
+
+        if (leftBound > rightBound) {
+            return null;
+        }
+
+        Node root = new Node(preOrder[preOrderIndex[0]]);
+        preOrderIndex[0]++;
+
+        int inOrderIndex = searchNodeInOrder(inOrder, preOrder[preOrderIndex[0] - 1], leftBound, rightBound);
+
+        root.leftNode = buildBinaryTree(preOrder, inOrder, preOrderIndex, leftBound, inOrderIndex - 1);
+        root.rightNode = buildBinaryTree(preOrder, inOrder, preOrderIndex, inOrderIndex + 1, rightBound);
+
+        return root;
+    }
+
+    public static int searchNodeInOrder(int inOrder[], int preOrderItem, int leftBound, int rightBound) {
+        for (int i = leftBound; i <= rightBound; i++) {
+            if (inOrder[i] == preOrderItem) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void postOrderTraversal(Node rootNode) {
+        if (rootNode == null)
+            return;
+
+        postOrderTraversal(rootNode.leftNode);
+        postOrderTraversal(rootNode.rightNode);
+        System.out.print(" " + rootNode.data);
+    }
+
+    public static void main(String args[]) {
+        int inOrder[] = { 2, 5, 4, 1, 3, 6 };
+        int preOrder[] = { 1, 2, 5, 4, 3, 6 };
+
+        int[] preOrderIndex = { 0 };
+        Node rootNode = buildBinaryTree(preOrder, inOrder, preOrderIndex, 0, inOrder.length - 1);
+
+        System.out.print("\n\n\nBinary Tree:\nPostOrder Traversal = [");
+        postOrderTraversal(rootNode);
+        System.out.println(" ]\n\n\n");
+    }
+
+}
