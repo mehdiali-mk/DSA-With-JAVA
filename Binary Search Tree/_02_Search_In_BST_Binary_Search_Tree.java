6d0140c5e4678727a0b79c7dd47bcd8e57af0f7d
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2026-02-08 10:08:04 +0530

    Search In BST Binary Search Tree java program is added.

---
+++ Binary Search Tree/_02_Search_In_BST_Binary_Search_Tree.java
@@ -0,0 +1,66 @@
+public class _02_Search_In_BST_Binary_Search_Tree {
+    static class Node {
+        int data;
+        Node leftNode;
+        Node rightNode;
+
+        Node(int data) {
+            this.data = data;
+            leftNode = null;
+            rightNode = null;
+        }
+    }
+
+    public static Node insertNode(Node rootNode, int data) {
+        if (rootNode == null) {
+            rootNode = new Node(data);
+            return rootNode;
+        }
+
+        if (rootNode.data > data) {
+            rootNode.leftNode = insertNode(rootNode.leftNode, data);
+        } else {
+            rootNode.rightNode = insertNode(rootNode.rightNode, data);
+        }
+
+        return rootNode;
+    }
+
+    public static void InOrderTraversal(Node rootNode) {
+        if (rootNode == null)
+            return;
+
+        InOrderTraversal(rootNode.leftNode);
+        System.out.print(rootNode.data + " ");
+        InOrderTraversal(rootNode.rightNode);
+    }
+
+    public static boolean searchInBST(Node rootNode, int number) {
+        if (rootNode == null) {
+            return false;
+        }
+
+        if (rootNode.data > number) {
+            searchInBST(rootNode.leftNode, number);
+        } else if (rootNode.data < number) {
+            searchInBST(rootNode.rightNode, number);
+        }
+
+        return true;
+    }
+
+    public static void main(String args[]) {
+        int values[] = { 5, 1, 3, 4, 2, 7 };
+        Node rootNode = null;
+
+        for (int i = 0; i < values.length; i++) {
+            rootNode = insertNode(rootNode, values[i]);
+        }
+
+        System.out.println("\n\n\n\nBinary Search Tree (InOrder):");
+        InOrderTraversal(rootNode);
+        System.out.println("\n\n");
+        System.out.print("Search in BST for (2): " + searchInBST(rootNode, 2));
+        System.out.println("\n\n\n\n");
+    }
+}
